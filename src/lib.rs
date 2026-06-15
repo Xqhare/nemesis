b3477352044fcc58e82e39d1a7e87a36f61809f6
Xqhare <xqhare@web.de>
2026-06-15 13:49:59 +0200

    cleanup(lib): remove boilerplate, clean up re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -27,20 +27,4 @@
 )]
 
 mod error;
-pub use error::{NemesisError, NemesisPayload, NemesisChainIter, NemesisCollection, NemesisResultExt};
-
-/// Adds two numbers
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use error::{NemesisChainIter, NemesisCollection, NemesisError, NemesisPayload, NemesisResultExt};
